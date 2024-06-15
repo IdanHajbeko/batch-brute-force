cc7ec7eb6d4b555a80ef3d4147a4624ace12bfd8
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2024-06-16 02:35:30 +0300

    Update brute_force.bat

--- brute_force.bat
+++ brute_force.bat
@@ -10,7 +10,7 @@ set YW=%ESC%[33m
 set BL=%ESC%[34m
 set WT=%ESC%[37m
 set RN=%ESC%[0m
-
+rem username - admin password - password
 set "hashed_username=c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec"
 set "hashed_password=b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86"
 
